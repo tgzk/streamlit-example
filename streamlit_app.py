b500445d3f7618bd16fa380940b6d690b44aa2ad
tgzk <79343857+tgzk@users.noreply.github.com>
2021-08-06 19:29:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,5 @@ with st.echo(code_location='below'):
         data.append(Point(x, y))
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#c968c9', opacity=0.5)
+        .mark_circle(color='#68c968', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
